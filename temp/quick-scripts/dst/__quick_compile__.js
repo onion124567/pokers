5ee4962b8f24cd10129d565deaebba953721b0fd
onion124567 <250124567@qq.com>
2020-11-22 10:03:31 +0800

    upte

--- temp/quick-scripts/dst/__quick_compile__.js
+++ temp/quick-scripts/dst/__quick_compile__.js
@@ -1,6 +1,6 @@
 
 (function () {
-var scripts = [{"deps":{"./assets/scripts/Player":5,"./assets/scripts/PokerUtil":4,"./assets/scripts/Star":3,"./assets/scripts/Card":6,"./assets/migration/use_v2.0.x_cc.Toggle_event":2,"./assets/scripts/Game":1},"path":"preview-scripts/__qc_index__.js"},{"deps":{"PokerUtil":4},"path":"preview-scripts/assets/scripts/Game.js"},{"deps":{},"path":"preview-scripts/assets/migration/use_v2.0.x_cc.Toggle_event.js"},{"deps":{},"path":"preview-scripts/assets/scripts/Star.js"},{"deps":{},"path":"preview-scripts/assets/scripts/PokerUtil.js"},{"deps":{},"path":"preview-scripts/assets/scripts/Player.js"},{"deps":{},"path":"preview-scripts/assets/scripts/Card.js"}];
+var scripts = [{"deps":{"./assets/migration/use_v2.0.x_cc.Toggle_event":2,"./assets/scripts/PokerUtil":3,"./assets/scripts/Star":4,"./assets/scripts/Player":5,"./assets/scripts/Card":6,"./assets/scripts/Game":1},"path":"preview-scripts/__qc_index__.js"},{"deps":{"PokerUtil":3},"path":"preview-scripts/assets/scripts/Game.js"},{"deps":{},"path":"preview-scripts/assets/migration/use_v2.0.x_cc.Toggle_event.js"},{"deps":{},"path":"preview-scripts/assets/scripts/PokerUtil.js"},{"deps":{},"path":"preview-scripts/assets/scripts/Star.js"},{"deps":{},"path":"preview-scripts/assets/scripts/Player.js"},{"deps":{},"path":"preview-scripts/assets/scripts/Card.js"}];
 var entries = ["preview-scripts/__qc_index__.js"];
 var bundleScript = 'preview-scripts/__qc_bundle__.js';
 
