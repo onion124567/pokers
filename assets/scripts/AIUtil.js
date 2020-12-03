3999fe91aa1b6ef5b139c4785cce6ba82a363997
wangcong <wangcong@autohome.com.cn>
2020-12-03 20:29:24 +0800

    加入部分逻辑

--- assets/scripts/AIUtil.js
+++ assets/scripts/AIUtil.js
@@ -14,16 +14,37 @@ export default class AIUtil {
    }
 
     /**
+     * 游戏每轮逻辑，
      * 赢家出牌，确定本轮主 将主放进卡片数组里 调sendAIHostCard
      * 下家出牌 调sendAICard
+     * 4家都出完结算，积分计算，结束本轮，返回积分
      */
    roundProgram(){
 
    }
+
+    /**
+     * 先手电脑逻辑
+     * 普通打法：
+     * 有副出最大的副牌
+     * 其次出最小主牌，不调主对
+     * 最后一轮出主对
+     * @param gameHost
+     * @param cardArray
+     */
    sendAIHostCard(gameHost,cardArray){
 
    }
-   sendAICard(gameHost,roundHost,userCard,cardArray){
+
+    /**
+     * 后手电脑逻辑
+     *
+     * @param gameHost
+     * @param roundHost
+     * @param userCard
+     * @param cardArray
+     */
+   sendAIFollowCard(gameHost,roundHost,userCard,cardArray){
 
    }
 
